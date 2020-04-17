ee340dcd0e2dab5f41a4b9aa31abae66727964fa
Adrián Díaz <onelifedesigning@gmail.com>
2020-04-17 02:05:05 +0200

    Creo la estructura inicial de los archivos y defino las variables globales

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+// GLOBAL VARIABLES
+// Define the Object groundGrid with all its values ​​and possibilities
+let groundGrid = {
+    horizontalLength: 10,
+    verticalLength: 10
+};
+
+// Define the Object rover with all its values ​​and possibilities
+let rover = {
+    direction: "N",
+    positionX: 0,
+    positionY: 0
+};
+// Define the Array directions with all possibilities
+let directions = ["N", "E", "S", "W"];
+
